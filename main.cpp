bf9e59a6e9feb9316b445a5a53adbea85ce36f7d
Leonardo Perez <83437994+Ldltorre14@users.noreply.github.com>
2023-04-04 17:23:41 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,30 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include "Particle.h"
 
 #define WIDTH 900
 #define HEIGHT 600
+#define FPS 60.f
 
 int main() {
 
 	sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Particles System Simulation");
-	window.setFramerateLimit(240);
-
+	window.setFramerateLimit(FPS);
+	Particle p1;
 
 	while (window.isOpen()) {
-		sf::Event event;
-		while (window.pollEvent(event)){
-			if (event.type == sf::Event::Closed) {
-				window.close();
-			}
+    sf::Event event;
+    while (window.pollEvent(event)){
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        }
+    }
 
-
-
-		}
-	}
+    // Update and draw the particle
+    window.clear();
+    p1.update(window);
+    p1.draw(window);
+    window.display();
+}
 
 }
